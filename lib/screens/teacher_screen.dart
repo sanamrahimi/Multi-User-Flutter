fc17c898473eb2d11ded08ed821a94a9d03b6098
sanamrahimi <sanam.rahimi@gmail.com>
2023-11-11 11:42:59 +0500

    add teacher screen

---
+++ lib/screens/teacher_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:multi_role_app/screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TeacherScreen extends StatefulWidget {
+  const TeacherScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TeacherScreen> createState() => _TeacherScreenState();
+}
+
+class _TeacherScreenState extends State<TeacherScreen> {
+  String email = '', age = '', type = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    email = sp.getString('email') ?? '';
+    age = sp.getString('age') ?? '';
+    type = sp.getString('userType') ?? '';
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Teacher view'),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Email'),
+              Text(email.toString()),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Age'),
+              Text(age.toString()),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('User Type'),
+              Text(type.toString()),
+            ],
+          ),
+          InkWell(
+            onTap: () async {
+              SharedPreferences sp = await SharedPreferences.getInstance();
+              sp.clear();
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()));
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Container(
+                height: 40,
+                width: double.infinity,
+                color: Colors.green,
+                child: Center(
+                    child: Text(
+                  'logOut',
+                  style: TextStyle(color: Colors.white),
+                )),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
